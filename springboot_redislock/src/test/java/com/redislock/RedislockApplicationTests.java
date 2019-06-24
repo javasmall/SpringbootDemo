9daf70f0b242aa9199782af212374983bec52772
javasmall <1315426911@qq.com>
2019-06-24 15:05:21 +0800

    分布式锁

---
+++ springboot_redislock/src/test/java/com/redislock/RedislockApplicationTests.java
@@ -0,0 +1,16 @@
+package com.redislock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedislockApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
